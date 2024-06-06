39088c432653412b6d82401eb480649cfcb7b730
Sudarshan <poudelsudarshan650@gmail.com>
2024-06-06 10:29:34 +0545

    name changed

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import pickle
 
 from is_duplicate import is_duplicate
 
-st.header('Quora - Duplicate question detection')
+st.title('AskAlign', anchor=False)
+st.text("'Quora - Duplicate question detection'")
+
 
 qn1 = st.text_input('Enter first question')
 qn2 = st.text_input('Enter second question')
 
-if st.button('Submit', type="primary"):
+if st.button('Checl', type="primary"):
     if(is_duplicate(qn1, qn2)):
         st.markdown('### Questions are duplicate' )
     else:
